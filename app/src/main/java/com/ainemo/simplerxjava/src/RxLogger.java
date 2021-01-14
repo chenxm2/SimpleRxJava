be967cf17395a989e68137752596fa99cc37c32f
chenxianming01 <chenxianming01@baidu.com>
2021-01-14 13:53:47 +0800

    rebase commit 1

--- app/src/main/java/com/ainemo/simplerxjava/src/RxLogger.java
+++ app/src/main/java/com/ainemo/simplerxjava/src/RxLogger.java
@@ -3,5 +3,6 @@ package com.ainemo.simplerxjava.src;
 import java.util.logging.Logger;
 
 public class RxLogger {
+    // rebase commit 1
     public static final Logger logger = Logger.getLogger("RxLogger");
 }
